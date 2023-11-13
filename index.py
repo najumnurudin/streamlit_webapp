5a9d794219062dbc621d14cbe1c561c2cb377e0f
najumnurudin <150137962+najumnurudin@users.noreply.github.com>
2023-11-13 02:25:55 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,40 @@
-import mysql.connector
-import streamlit as st
-import pandas as pd
+try:
+    import os
+    import sys
+    import streamlit as st
+    import pandas as pd
+    from io import BytesIO, StringIO
+    print("all Modules are loaded")
+except Exception as e:
+    print("Some modules are missing : {}" .format(e))
+    
+    
+    # css
+    
+STYLE = """
+        <style>
+        img {
+            max-widt: 100;
+        }
+        </style>
+        """
 
-# Connect to the MySQL database
-cnx = mysql.connector.connect(user='root', password='',
-                              host='localhost', database='streamlit')
-cursor = cnx.cursor()
-
-
-# Execute the query to retrieve all rows
-query = "SELECT * FROM data"
-cursor.execute(query)
-
-# Fetch all rows
-rows = cursor.fetchall()
-
-# Convert the rows to a pandas dataframe
-df = pd.DataFrame(rows, columns=[i[0] for i in cursor.description])
-
-# Display the dataframe using Streamlit
-st.dataframe(df)
+def main():
+    """Run the function to display the Streamlit app"""
+    st.info(__doc__)
+    st.markdown(STYLE, unsafe_allow_html=True)
+    file = st.file_uploader("Upload file", type=["csv", "png", "peg", "gif"])
+    show_file = st.empty()
+    
+    if not file:
+        show_file.info("Please Upload a file : {}".format(''.join(["csv", "png","peg", "gif"])))
+        return
+    content = file.getvalue()
+    
+    if isinstance(file, BytesIO):
+        show_file.image(file)
+    else:
+        df = pd.read_csv(file)
+        st.dataframe(data.head(10))
+        
+main()
